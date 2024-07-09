ab6ebe0ddf3ae212364d79afa4922c90228a3474
mwdemos <159658362+mwdemos@users.noreply.github.com>
2024-07-09 10:01:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const url = "https://public.tableau.com/views/MCASScores_17204477306580/2023PVPA
 
 const vizContainer = document.getElementById('vizContainer');
 const options = {
-    hideTabs: true,
+    hideTabs: false,
     height: 1000,
     width: 1200,
     onFirstInteraction: function() {
